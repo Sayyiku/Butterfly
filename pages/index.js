165961736924db8ea97bd403216865ac62bc5783
“sayyiku” <liricarain@gmail.com>
2022-05-02 23:16:07 +0800

    :tada: (Begin): Begin
    Begin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: Sayyiku
+ * @Date: 2022-04-20 12:26:14
+ * @LastEditors: Sayyiku
+ * @LastEditTime: 2022-04-20 13:39:35
+ * @FilePath: \pika\pages\index.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by Sayyiku, All Rights Reserved. 
+ */
 import Main from "components/main";
-
 export default function Home() {
   return (
     <>
